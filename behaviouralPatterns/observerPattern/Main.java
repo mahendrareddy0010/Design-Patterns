1c6370da29734e546367e0315a7283b7c14fc112
Mahendra <mahendrareddyyarramreddy@gmail.com>
2024-03-31 12:28:29 +0530

    suppressed warinings in Observer pattern

--- behaviouralPatterns/observerPattern/Main.java
+++ behaviouralPatterns/observerPattern/Main.java
@@ -6,8 +6,11 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Random random = new Random();
         WeatherStation weatherStation = new WeatherStation();
+        @SuppressWarnings("unused")
         TemparatureDisplay temparatureDisplay = new TemparatureDisplay(weatherStation);
+        @SuppressWarnings("unused")
         HumidityDisplay humidityDisplay = new HumidityDisplay(weatherStation);
+        @SuppressWarnings("unused")
         PressureDisplay pressureDisplay = new PressureDisplay(weatherStation);
 
         weatherStation.setMeasurements(random.nextDouble(30, 50), random.nextDouble(100, 150), random.nextDouble(500, 600));
